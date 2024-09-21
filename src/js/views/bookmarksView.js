6998c4437d040e8c30f7e53ccff9799f4a5f9279
eid-osama <eid.osama39@gmail.com>
2024-09-22 00:58:09 +0300

    fix paths

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import previewview from './previewView.js';
-import icons from 'url:../../img/icons.svg';
+import icons from '../../img/icons.svg';
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import previewview from './previewView.js';
-import icons from 'url:../../img/icons.svg';
+import icons from '../../img/icons.svg';
 class ResultView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = 'No Recipes found!';
