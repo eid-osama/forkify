d1d1f240b6bd4b09adcb5749659c2e9de381d93f
eid-osama <eid.osama39@gmail.com>
2024-09-22 01:25:29 +0300

    fixing upload2

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'c103371b-2223-4399-950d-d639ddc0c5ea';
+export const KEY = '250416eb-8b4d-4e9e-a211-2ff651c5b667';
 export const MODEL_CLOSE_SEC = 2.5;
