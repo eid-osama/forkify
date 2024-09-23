d16ca5c4d9ce5ada1c73d3fba98c90da0c66f2ff
eid-osama <eid.osama39@gmail.com>
2024-09-23 19:50:33 +0300

    final version

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '70392831-9e70-4dff-b74d-3dfe7e77aceb';
+export const KEY = '0f864984-3d94-4dca-b2c3-618507052368';
 export const MODAL_CLOSE_SEC = 2.5;
